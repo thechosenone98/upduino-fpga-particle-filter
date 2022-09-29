d0e8603541e4222659b04f3f913380c1c084529e
thechosenone98 <zacfire1998@hotmail.fr>
2022-09-29 18:31:31 +0200

    top level for particle filter (placeholder)

---
+++ src/particle_filter_state_machine/particle_filter_top.v
@@ -0,0 +1,13 @@
+module particle_filter #(
+    parameter string START_MESSAGE = "START_PARTUPDATE",
+    parameter string START_MAP_MESSAGE = "START_MAPUPDATE",
+) (
+    input serial_in,
+    output serial_out,
+    input clk,
+    input reset,
+);
+    
+    wire start_of_particle_message
+
+endmodule
