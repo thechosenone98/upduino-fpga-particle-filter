f59901d5006bb488bddfe06fc194d360fe93310b
thechosenone98 <zacfire1998@hotmail.fr>
2022-09-29 19:13:06 +0200

    Fixed state machine (reworked the design)

--- src/msg_receiver/msg_receiver_module/serial_msg_receiver.v
+++ src/msg_receiver/msg_receiver_module/serial_msg_receiver.v
@@ -36,7 +36,7 @@ module serial_msg_receiver #(
 	localparam START_MESSAGE_COUNTER_WIDTH = MinBitWidth(START_MESSAGE_LENGHT);
 	reg [7:0] rx_data_reg;
 	reg data_read;
-	reg new_data;
+	reg data_processed;
 	reg [2:0] curr_state;
 	reg [2:0] next_state;
 	reg [DATA_MESSAGE_COUNTER_WIDTH - 1:0] data_countdown;
@@ -48,13 +48,13 @@ module serial_msg_receiver #(
 		case (curr_state)
 			3'd0:
 			begin
-				if (!data_read) begin
+				if (rx_data_ready) begin
 					next_state = 3'd1;
 				end
 			end
 			3'd1:
 			begin
-				if (new_data && rx_data_reg == START_PARTICLE_MESSAGE[START_PARTICLE_MESSAGE_WIDTH - (counter << 3) - 1 -: 8] && rx_data_reg != START_MAP_MESSAGE[START_MAP_MESSAGE_WIDTH - (counter << 3) - 1 -: 8]) begin
+				if (data_processed && rx_data_reg == START_PARTICLE_MESSAGE[START_PARTICLE_MESSAGE_WIDTH - (counter << 3) - 1 -: 8] && rx_data_reg != START_MAP_MESSAGE[START_MAP_MESSAGE_WIDTH - (counter << 3) - 1 -: 8]) begin
 					next_state = 3'd2;
 				end else if (rx_data_reg == START_MAP_MESSAGE[START_MAP_MESSAGE_WIDTH - (counter << 3) - 1 -: 8] && rx_data_reg != START_PARTICLE_MESSAGE[START_MAP_MESSAGE_WIDTH - (counter << 3) - 1 -: 8]) begin
 					next_state = 3'd3;
@@ -62,14 +62,14 @@ module serial_msg_receiver #(
 			end
 			3'd2:
 			begin
-				if (new_data && rx_data_reg != START_PARTICLE_MESSAGE[START_PARTICLE_MESSAGE_WIDTH - (counter << 3) - 1 -: 8]) begin
+				if (data_processed && rx_data_reg != START_PARTICLE_MESSAGE[START_PARTICLE_MESSAGE_WIDTH - (counter << 3) - 1 -: 8]) begin
 					next_state = 3'd0;
 				end else if (counter == START_PARTICLE_MESSAGE_LENGTH_BYTE - 1) begin
 					next_state = 3'd4;
 				end
 			end
 			3'd3:
-				if (new_data && rx_data_reg != START_MAP_MESSAGE[START_MAP_MESSAGE_WIDTH - (counter << 3) - 1 -: 8]) begin
+				if (data_processed && rx_data_reg != START_MAP_MESSAGE[START_MAP_MESSAGE_WIDTH - (counter << 3) - 1 -: 8]) begin
 					next_state = 3'd0;
 				end else if (counter == START_MAP_MESSAGE_LENGTH_BYTE - 1) begin
 					next_state = 3'd4;
@@ -87,17 +87,18 @@ module serial_msg_receiver #(
 		endcase
 	end
 	
-	// Register serial data when ready, ALWAYS!
-	always @(posedge clk) begin
-		if(rx_data_ready == 1'b1 && data_read == 1'b1) begin
-			rx_data_reg <= rx_data;
-			data_read <= 1'b0;
-		end else if (new_data == 1'b1) begin
-			data_read <= 1'b1;
-		end else if (curr_state == 3'd0) begin
-			data_read <= 1'b1;
-		end
-	end
+	// // Register serial data when ready, ALWAYS!
+	// always @(posedge clk) begin
+	// 	if(rx_data_ready == 1'b1 && data_read == 1'b1) begin
+	// 		rx_data_reg <= rx_data;
+	// 		data_read <= 1'b0;
+	// 	end else if(!rx_data_ready && data_processed) begin
+	// 		data_read <= 1'b1;
+	// 		data_processed = 1'b0;
+	// 	end else if(curr_state == 'd0) begin
+	// 		data_read <= 1'b1;
+	// 	end
+	// end
 
 	// Output part of the FSM
 	always @(posedge clk or posedge reset) begin : FSM_SEQ
@@ -106,60 +107,92 @@ module serial_msg_receiver #(
 			counter <= 'b0;
 			particle_data_flag <= 1'b0;
 			map_data_flag <= 1'b0;
-			new_data <= 1'b0;
+			data_processed <= 1'b0;
+			data_read <= 1'b0;
 		end
 		else begin
 			curr_state <= next_state;
-			new_data <= 1'b0;
 			case (curr_state)
 				3'd0: begin
 					counter <= 'b0;
 					particle_data_flag <= 1'b0;
 					map_data_flag <= 1'b0;
+					data_read <= 1'b0;
+					data_processed <= 1'b0;
+					if(curr_state != next_state) begin
+						rx_data_reg <= rx_data;
+					end
 				end
 				3'd1:
 				begin
-					if (!data_read) begin
+					if (rx_data_ready && !data_read) begin
+						rx_data_reg <= rx_data;
+						data_read <= 1'b1;
+					end else if (data_read && !data_processed) begin
 						counter <= counter + 1'b1;
-						new_data <= 1'b1;
+						data_processed <= 1'b1;
+					end else if(data_processed && !rx_data_ready) begin
+						data_read <= 1'b0;
+						data_processed <= 1'b0;
 					end
 				end
 				3'd2: 
 				begin
 					data_countdown <= PARTICLE_MESSAGE_LENGHT;
 					particle_data_flag  <= 1'b1;
-					if (!data_read) begin
+					if (rx_data_ready && !data_read) begin
+						rx_data_reg <= rx_data;
+						data_read <= 1'b1;
+					end else if (data_read && !data_processed) begin
 						counter <= counter + 1'b1;
-						new_data <= 1'b1;
+						data_processed <= 1'b1;
+					end else if(data_processed && !rx_data_ready) begin
+						data_read <= 1'b0;
+						data_processed <= 1'b0;
 					end
 				end
 				3'd3:
 				begin
 					data_countdown <= MAP_MESSAGE_LENGHT;
 					map_data_flag  <= 1'b1;
-					if (!data_read) begin
+					if (rx_data_ready && !data_read) begin
+						rx_data_reg <= rx_data;
+						data_read <= 1'b1;
+					end else if (data_read && !data_processed) begin
 						counter <= counter + 1'b1;
-						new_data <= 1'b1;
+						data_processed <= 1'b1;
+					end else if(data_processed && !rx_data_ready) begin
+						data_read <= 1'b0;
+						data_processed <= 1'b0;
 					end
 				end
 				3'd4:
 				begin
-					if (!data_read) begin
+					if (rx_data_ready && !data_read) begin
+						rx_data_reg <= rx_data;
+						data_read <= 1'b1;
+					end else if (data_read && !data_processed) begin
 						msg_out <= rx_data_reg;
 						data_countdown <= data_countdown - 1'b1;
-						new_data <= 1'b1;
+						data_processed <= 1'b1;
+					end else if(data_processed && !rx_data_ready) begin
+						data_read <= 1'b0;
+						data_processed <= 1'b0;
 					end
 				end
 				default:
 				begin
 					curr_state <= 3'd0;
 					counter <= 'b0;
-					new_data <= 1'b0;
+					data_processed <= 1'b0;
+					particle_data_flag <= 1'b0;
+					map_data_flag <= 1'b0;
+					data_read <= 1'b0;
 				end
 			endcase
 		end
 	end
 
-	assign data_valid = new_data && (curr_state == 3'd4);
+	assign data_valid = data_processed && (curr_state == 3'd4);
 
 endmodule
