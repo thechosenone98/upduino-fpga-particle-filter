2f65224635a381e0f1b2ed873752aac9169bc945
thechosenone98 <zacfire1998@hotmail.fr>
2022-09-30 20:12:42 +0200

    Added clock divider to top module

--- src/msg_receiver/msg_receiver_echo_test/msg_receiver_echo_top.v
+++ src/msg_receiver/msg_receiver_echo_test/msg_receiver_echo_top.v
@@ -1,6 +1,7 @@
 `include "../msg_receiver_module/serial_msg_receiver.v"
-`include "../../serial_port/serial_module/serial_tx.v"
 `include "../../serial_port/serial_module/serial_rx.v"
+`include "../../serial_port/serial_module/serial_tx.v"
+`include "../../clock_divider_module/clock_divider_pulse.v"
 
 module msg_receiver_echo (
     input ser_rx,
@@ -41,14 +42,24 @@ module msg_receiver_echo (
     defparam rgb.RGB2_CURRENT = "0b000001";
     // LED MODULE --------------------------------------------------------------
 
+    //CLOCK STROBE MODULE ------------------------------------------------------
+    wire clk_en;
+    clock_divider_pulse #(.N(2)) cdp
+    (
+        .CLK(clk),
+        .PULSE(clk_en)
+    );
+    //CLOCK STROBE MODULE ------------------------------------------------------
+
+
     // SERIAL RX MODULE --------------------------------------------------------
     wire [7:0] serial_buff_rx;
     wire [7:0] serial_buff_tx;
     wire data_valid_rx;
     wire data_valid_tx;
 
-    serial_rx #(.CLOCK_FREQUENCY(48000000),.BAUDRATE(115200)) s_rx  (
-        .i_Clock(clk),
+    serial_rx #(.CLOCK_FREQUENCY(24000000),.BAUDRATE(576000)) s_rx  (
+        .i_Clock(clk_en),
         .i_Rx_Serial(ser_rx),
         .o_Rx_Byte(serial_buff_rx),
         .o_Rx_DV(data_valid_rx)
@@ -62,10 +73,9 @@ module msg_receiver_echo (
         .START_MAP_MESSAGE("FGHIJ"),
         .START_MAP_MESSAGE_LENGTH_BYTE(5),
         .PARTICLE_MESSAGE_LENGHT(8),
-        .MAP_MESSAGE_LENGHT(16)
+        .MAP_MESSAGE_LENGHT(8)
     ) smr (
-        .clk(clk),
-        .reset(1'b0),
+        .clk(clk_en),
         .rx_data(serial_buff_rx),
         .rx_data_ready(data_valid_rx),
         .msg_out(serial_buff_tx),
@@ -76,8 +86,8 @@ module msg_receiver_echo (
     // MSG RECEIVER MODULE -----------------------------------------------------
 
     // SERIAL TX MODULE --------------------------------------------------------
-    serial_tx #(.CLOCK_FREQUENCY(48000000),.BAUDRATE(115200)) s_tx  (
-        .i_Clock(clk),
+    serial_tx #(.CLOCK_FREQUENCY(24000000),.BAUDRATE(576000)) s_tx  (
+        .i_Clock(clk_en),
         .i_Tx_Byte(serial_buff_tx),
         .i_Tx_DV(data_valid_tx),
         .o_Tx_Serial(ser_tx),
